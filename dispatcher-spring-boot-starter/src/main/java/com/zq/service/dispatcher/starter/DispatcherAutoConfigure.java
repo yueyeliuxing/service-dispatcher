2e850b9798ff06ab642b4e305e1202bbe0fb8aab
zhouqi <zhouqi1@dainwoba.com>
2019-07-02 21:11:33 +0800

    service-dispatcher-starter

---
+++ dispatcher-spring-boot-starter/src/main/java/com/zq/service/dispatcher/starter/DispatcherAutoConfigure.java
@@ -0,0 +1,27 @@
+package com.zq.service.dispatcher.starter;
+
+import com.zq.service.dispatcher.AnnotationServiceDispatcher;
+import com.zq.service.dispatcher.ServiceDispatcher;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.Resource;
+
+/**
+ * @program: service-dispatcher
+ * @description: 自动配置
+ * @author: zhouqi1
+ * @create: 2019-07-02 20:49
+ **/
+@Configuration
+@ConditionalOnClass(AnnotationServiceDispatcher.class)
+public class DispatcherAutoConfigure {
+
+    @Bean
+    @ConditionalOnMissingBean
+    ServiceDispatcher serviceDispatcher (){
+        return  new AnnotationServiceDispatcher();
+    }
+}
